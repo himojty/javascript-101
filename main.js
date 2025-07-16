865f793a8b82b4f48d946aa9c7a032ad22a5349e
himojty <mtryrd.13@gmail.com>
2025-07-16 13:25:35 +0300

    reboot

--- main.js
+++
@@ -1,7 +0,0 @@
-let promise = await fetch("http://147.45.223.161:8000/api-v1/articles/");
-let json = await promise.json();
-console.log(json);
-
-let promise1 = await fetch("http://147.45.223.161:8000/api-v1/items/");
-let json1 = await promise1.json();
-console.log(json1);
