4b824c81293d0bf0d7a8c2c11473b0d372c383e4
himojty <mtryrd.13@gmail.com>
2025-07-16 12:01:47 +0300

    change url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let promise = await fetch("http://127.0.0.1:8000/api-v1/articles/");
+let promise = await fetch("http://147.45.223.161:8000/api-v1/articles/");
 let json = await promise.json();
 console.log(json);
 
-let promise1 = await fetch("http://127.0.0.1:8000/api-v1/items/");
+let promise1 = await fetch("http://147.45.223.161:8000/api-v1/items/");
 let json1 = await promise1.json();
 console.log(json1);
